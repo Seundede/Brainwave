f9e4d646281b32b59e721f2415d87ade35830ddd
Seundede <omadoyedede0@gmail.com>
2024-04-07 22:35:38 +0100

    feat: button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ButtonSvg from "../../public/images/common/ButtonSvg";
+
+interface ButtonProps {
+  handleClick: () => void;
+  text: string;
+}
+const Button: React.FC<ButtonProps> = ({ handleClick, text }) => (
+  <button
+    className="button relative inline-flex items-center justify-center h-11 px-7 m-10 text-xs font-bold uppercase tracking-wider hover:text-gray-300 cursor-pointer"
+    onClick={handleClick}
+  >
+    {text}
+    {ButtonSvg()}
+  </button>
+);
+
+export default Button;
