83904b4e72e1b104db3f5a2a74bc81b79571d4b0
Seundede <omadoyedede0@gmail.com>
2024-04-08 18:13:18 +0100

    chore: settings.json and lint fix

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ interface ButtonProps {
 }
 const Button: React.FC<ButtonProps> = ({ handleClick, text }) => (
   <button
-    className="button relative inline-flex items-center justify-center h-11 px-7 m-10 text-xs font-bold uppercase tracking-wider hover:text-gray-300 cursor-pointer"
+    className="relative inline-flex items-center justify-center h-11 px-7 m-10 text-xs font-bold uppercase tracking-wider hover:text-gray-300 cursor-pointer"
     onClick={handleClick}
   >
     {text}
